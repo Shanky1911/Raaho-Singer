dd902ae69b2ac6a57addb45f7a5e34bac09f6994
Shanky1911 <125724915+Shanky1911@users.noreply.github.com>
2023-06-15 18:38:55 +0530

    Update target_bigquery.py

--- target_bigquery.py
+++ target_bigquery.py
@@ -1,33 +1,29 @@
+import logging
 import singer
-from google.cloud import bigquery
-from google.cloud.exceptions import NotFound
 import json
-import sys
-import psycopg2
 import datetime
+import sys
 from google.cloud import bigquery
+import datetime
+import pytz
+from google.cloud.exceptions import NotFound
 
-def convert_pgadmin_to_bigquery(data_type):
+REQUIRED_CONFIG_KEYS = ['keyfile_path', 'project_id', 'dataset_id']
+
+
+def convert_pgadmin_to_bigquery(data_type,udt_name):
     data_type = data_type.lower()
-    if data_type.startswith('int'):
-        return 'INT64'
-    elif data_type.startswith('serial') or data_type.startswith('bigserial'):
-        return 'INT64'
-    elif data_type.startswith('bigint'):
-        return 'INT64'
-    elif data_type.startswith('smallint'):
+    if data_type.startswith('int') or data_type.startswith('serial') or data_type.startswith('bigserial') or data_type.startswith('bigint') or data_type.startswith('smallint'):
         return 'INT64'
     elif data_type.startswith('numeric') or data_type.startswith('decimal'):
         return 'NUMERIC'
-    elif data_type.startswith('float') or data_type.startswith('double'):
-        return 'FLOAT64'
-    elif data_type.startswith('real'):
+    elif data_type.startswith('float') or data_type.startswith('double') or data_type.startswith('real'):
         return 'FLOAT64'
     elif data_type.startswith('boolean'):
         return 'BOOL'
-    elif data_type.startswith('character') or data_type.startswith('char') or data_type.startswith('bpchar'):
-        return 'STRING'
-    elif data_type.startswith('text') or data_type.startswith('citext'):
+    elif data_type.startswith('json'):
+        return 'JSONB'
+    elif data_type.startswith('character') or data_type.startswith('char') or data_type.startswith('bpchar') or data_type.startswith('text') or data_type.startswith('citext'):
         return 'STRING'
     elif data_type.startswith('date'):
         return 'DATE'
@@ -39,28 +35,82 @@ def convert_pgadmin_to_bigquery(data_type):
         return 'STRING'
     elif data_type.startswith('bytea') or data_type.startswith('blob'):
         return 'BYTES'
+    elif data_type == 'array':
+        # Check for specific array types
+        if 'int' in udt_name:
+            return 'INT64'
+        elif 'timestamp' in data_type:
+            return 'TIMESTAMP'
+        elif 'varchar' in data_type:
+            return 'STRING'
+        elif 'numeric' in data_type:
+            return 'NUMERIC'
+        elif 'bool' in data_type:
+            return 'BOOL'
+        elif 'date' in data_type:
+            return 'DATE'
+        elif 'time' in data_type:
+            return 'TIME'
+        elif 'float' in data_type:
+            return 'FLOAT64'
+        elif 'bytea' in data_type:
+            return 'BYTES'
+        elif 'record' in udt_name:
+            # Extract the nested fields of the record array
+            record_fields = udt_name[udt_name.find('(') + 1:udt_name.rfind(')')]
+            nested_schema = []
+            for field in record_fields.split(','):
+                field_name, field_type = field.strip().split(':')
+                field_schema = bigquery.SchemaField(field_name.strip(), convert_pgadmin_to_bigquery(field_type.strip()))
+                nested_schema.append(field_schema)
+            return 'RECORD' + str(nested_schema)
+        else:
+            return 'STRING'
     else:
         return 'STRING'
-    
 
 
-def get_schema_fields_from_catalog(stream_index):
+# Function to get schema fields from catalog
+
+
+# def get_schema_fields_from_catalog(stream_index):
+#     with open('catalog.json', 'r') as file:
+#         catalog_data = json.load(file)
+#     property_array = catalog_data['streams'][stream_index]['metadata']
+#     schema_fields = []
+#     for property_obj in property_array:
+#         property_name = property_obj['breadcrumb'][1]
+#         data_type = property_obj['metadata']['type']
+#         data_type = convert_pgadmin_to_bigquery(data_type)
+#         schema_fields.append(bigquery.SchemaField(property_name, data_type))
+#     return schema_fields
+
+
+jsonb_columns = {""}
+def get_schema_fields_from_catalog2(stream_index):
     with open('catalog.json', 'r') as file:
         catalog_data = json.load(file)
     property_array = catalog_data['streams'][stream_index]['metadata']
     schema_fields = []
     for property_obj in property_array:
-        property = property_obj['breadcrumb'][1]
+        # print(property_obj)
+        property_name = property_obj['breadcrumb'][1]
         data_type = property_obj['metadata']['type']
-        # data_type = convert_pgadmin_to_bigquery(data_type)
-        if(data_type == 'character varying'):
-            data_type = 'STRING'
-        elif(data_type == 'timestamp without time zone'):
-            data_type = "TIMESTAMP"
-        schema_fields.append(bigquery.SchemaField(property, data_type))
+        udt_name = property_obj['metadata']['udt_name']
+
+        if data_type == 'ARRAY':  # array-like type
+            inner_type = convert_pgadmin_to_bigquery(data_type,udt_name)  # extract inner type recursively
+            schema_fields.append(bigquery.SchemaField(property_name, inner_type, mode='REPEATED'))
+        elif data_type == 'jsonb' or data_type == 'json':
+            schema_fields.append(bigquery.SchemaField(property_name, 'STRING', mode='REPEATED'))
+            jsonb_columns.add(property_name)
+        else:
+            data_type = convert_pgadmin_to_bigquery(data_type,"")
+            schema_fields.append(bigquery.SchemaField(property_name, data_type))
     return schema_fields
 
 
+# Function to connect to BigQuery
 def connect_to_bigquery(config):
     keyfile_path = config['keyfile_path']
     project_id = config['project_id']
@@ -70,60 +120,121 @@ def connect_to_bigquery(config):
     return client, dataset_ref
 
 
-def record_exists(client, table_ref, record):
-    # Constructing a query to check if the record already exists in BigQuery
-    record_id = record['id']
-    query = f"SELECT COUNT(*) FROM `{table_ref.project}.{table_ref.dataset_id}.{table_ref.table_id}` WHERE id = {record_id}"
-    query_job = client.query(query)
-    
-    result = query_job.result()
-    row_count = next(result)[0]
-    return row_count
+# Function to load records into BigQuery table
+# Working code with flattening
+def load_records_to_bigquery(client, dataset_ref, table, records, stream_index):
+    # Flatten nested fields in records
+    flattened_records = []
+    # for record in records:
+    #     flattened_record = flatten_nested_fields(record)
+    #     flattened_records.append(flattened_record)
+    flattened_records = records
 
-
-def load_record_to_bigquery(client, dataset_ref, table, record,stream_index):
     table_ref = dataset_ref.table(table)
-    
+    temp_table_id = f"temp_table_{table}"
+    temp_table_ref = dataset_ref.table(temp_table_id)
+
+    primary_key_fields = ['id']  # Primary key field(s)
+
+
     try:
         bq_table = client.get_table(table_ref)
         schema = bq_table.schema
     except NotFound:
-
-        # Defining the schema fields
-        schema_fields = get_schema_fields_from_catalog(stream_index)
-
-        # Creating the table with the specified schema
+        schema_fields = get_schema_fields_from_catalog2(stream_index)
         table = bigquery.Table(table_ref, schema=schema_fields)
         bq_table = client.create_table(table)
         schema = bq_table.schema
+    
+    # creating temporary table if not exists
+    try:
+        temp_table = client.get_table(temp_table_ref)
+        # schema = bq_table.schema
+    except NotFound:
+        schema_fields = get_schema_fields_from_catalog2(stream_index)
+        temp_table = bigquery.Table(temp_table_ref, schema=schema_fields)
+        temp_table = client.create_table(temp_table,exists_ok=True)
+        # schema = bq_table.schema
 
-    record_count = record_exists(client,table_ref,record)
-    if not record_count >= 1:
+    # Calculate expiration time for the temporary table
+    expiration_time = datetime.datetime.now(pytz.UTC) + datetime.timedelta(minutes=1)
 
-        # Convert datetime objects to ISO 8601 formatted strings
+    # Convert expiration_time to Unix timestamp
+    expiration_timestamp = int(expiration_time.timestamp())
+
+    # Convert datetime objects to ISO 8601 formatted strings
+    for record in records:
         for field in schema:
+            # print(field)
             if field.field_type == 'TIMESTAMP':
                 field_name = field.name
                 if field_name in record:
                     value = record[field_name]
                     if isinstance(value, datetime.datetime):
                         record[field_name] = value.isoformat()
-        
-        # Loading the rows into BigQuery
-        if bq_table:
-            errors = client.insert_rows(table_ref, [record], selected_fields=schema)
+            if field.name in jsonb_columns and field.name in record:
+                print(record[field.name])
+                json_string = json.dumps(record[field.name])
+                print(json_string)
+                record[field.name] = json_string
+
+
+    batch_size = 1000
+    for i in range(0, len(flattened_records), batch_size):
+        batch_records = flattened_records[i:i + batch_size]
+
+        # Insert rows into temporary table
+        if temp_table:
+            logging.info(f"Syncing records for stream: {table_ref.table_id}")
+            errors = client.insert_rows(temp_table_ref, batch_records, selected_fields=schema)
+            logging.info("Last sync time: {}".format(datetime.datetime.now()))
+
 
         if errors:
+            print(errors)
             for error in errors:
                 print(error)
                 error_message = error.get('message', 'Unknown error')
                 singer.write_record(table, {"error": error_message})
-        else:
-            # Convert the datetime to string representation before writing state
-            last_synced_at = singer.utils.now().isoformat()
-            singer.write_state({"last_synced_at": last_synced_at})
 
 
+    print("==================================================================================")
+    print(f'''`{table_ref.project}.{table_ref.dataset_id}.{table_ref.table_id}`''')
+    print(f'''`{temp_table_ref.project}.{temp_table_ref.dataset_id}.{temp_table_ref.table_id}`''')
+    print("==================================================================================")
+
+    merge_query = f"""
+        MERGE `{table_ref.project}.{table_ref.dataset_id}.{table_ref.table_id}` AS T
+        USING (
+            SELECT *
+            FROM (
+                SELECT *,
+                    ROW_NUMBER() OVER (PARTITION BY {', '.join(primary_key_fields)} ORDER BY {', '.join(primary_key_fields)}) AS row_number
+                FROM `{temp_table_ref.project}.{temp_table_ref.dataset_id}.{temp_table_ref.table_id}`
+            )
+            WHERE row_number = 1
+        ) AS S
+        ON {f"{' AND '.join(f'T.{field} = S.{field}' for field in primary_key_fields)}"}
+        WHEN MATCHED THEN
+            UPDATE SET {', '.join(f"T.{field.name} = S.{field.name}" for field in schema if field.name not in primary_key_fields)}
+        WHEN NOT MATCHED THEN
+            INSERT ({', '.join(field.name for field in schema)})
+            VALUES ({', '.join(f"S.{field.name}" for field in schema)})
+    """
+
+    query_job = client.query(merge_query)
+    query_job.result()
+
+
+    print()
+    print("==================================================================================================")
+
+    print("Data loaded successfully.")
+    expiration_job = client.delete_table(temp_table_ref, not_found_ok=True)
+    # expiration_job.result()
+
+
+# Main function
 def main():
     args = singer.utils.parse_args(required_config_keys=[])
     config = args.config
@@ -132,20 +243,53 @@ def main():
 
     stream_name = ""
     stream_index = -1
+    records = []
+
+    date_format = '%Y-%m-%dT%H:%M:%S.%f'
+
+    # Get start time
+    start_time = datetime.datetime.now()
+    logging.info(f"Sync started at: {start_time}")
+
 
     for line in sys.stdin:
         message = json.loads(line)
-        # print(message)
         if message['type'] == 'RECORD':
             if message['stream'] != stream_name:
-                stream_index += 1
+                if stream_name != "":
+                    # Load any remaining records for the previous stream
+                    logging.info(f"Loading records into BigQuery table: {stream_name}")
+                    load_records_to_bigquery(client, dataset_ref, stream_name, records, stream_index)
+                    logging.info("Last sync time: {}".format(datetime.datetime.now()))
+
                 stream_name = message['stream']
+                stream_index += 1
+                records = []
+
             table = message['stream']
             record = message['record']
-            load_record_to_bigquery(client, dataset_ref, table, record,stream_index)
-    
+
+
+            records.append(record)
+
+    if stream_name != "":
+        logging.info(f"Loading records into BigQuery table: {stream_name}")
+        # Load any remaining records for the last stream
+        load_records_to_bigquery(client, dataset_ref, table, records, stream_index)
+        logging.info("Last sync time: {}".format(datetime.datetime.now()))
+
+    # Get end time
+    end_time = datetime.datetime.now()
+    logging.info(f"Sync ended at: {end_time}")
+
+    # Calculate and log the program execution time
+    execution_time = end_time - start_time
+    logging.info(f"Sync execution time: {execution_time}")
+
+    print("==================================================================================================")
     client.close()
 
 
 if __name__ == '__main__':
     main()
+
